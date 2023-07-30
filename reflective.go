4ef980473832eb9ee942005f33fe3cdfa4dfdac4
Merlin Cox <merlin.cox@denma.digital>
2023-07-30 21:45:07 +0100

    add by value

--- reflective.go
+++ reflective.go
@@ -19,46 +19,54 @@ func randString() string {
 func FillRandomly(a any) error {
 	val := reflect.ValueOf(a)
 	if val.Kind() != reflect.Pointer {
-		return fmt.Errorf("the argument FillRandomly to must be a pointer")
+		return fmt.Errorf("the argument to FillRandomly to must be a pointer")
 	}
 
 	return fillRandomly(val.Elem())
 }
 
-func fillRandomly(receiver reflect.Value) error {
-	if !receiver.CanSet() {
+func FillRandomlyByValue(val reflect.Value) error {
+	if !val.CanSet() {
+		return fmt.Errorf("the argument to FillRandomlyByValue must be able to be set")
+	}
+
+	return fillRandomly(val)
+}
+
+func fillRandomly(val reflect.Value) error {
+	if !val.CanSet() {
 		return nil
 	}
 
-	switch receiver.Kind() {
+	switch val.Kind() {
 	case reflect.Ptr:
-		if receiver.IsNil() {
-			receiver.Set(reflect.New(receiver.Type().Elem()))
+		if val.IsNil() {
+			val.Set(reflect.New(val.Type().Elem()))
 		}
-		return fillRandomly(receiver.Elem())
+		return fillRandomly(val.Elem())
 
 	case reflect.Bool:
 		randBool := (rand.Int() % 2) == 0
-		receiver.SetBool(randBool)
+		val.SetBool(randBool)
 
 	case reflect.Int, reflect.Int16, reflect.Int32, reflect.Int64:
 		randInt := int64(rand.Intn(204800))
-		receiver.SetInt(randInt)
+		val.SetInt(randInt)
 
 	case reflect.Uint16, reflect.Uint32, reflect.Uint64:
 		randUint := uint64(rand.Uint32())
-		receiver.SetUint(randUint)
+		val.SetUint(randUint)
 
 	case reflect.Float32, reflect.Float64:
 		randFloat := float64(rand.Float32())
-		receiver.SetFloat(randFloat)
+		val.SetFloat(randFloat)
 
 	case reflect.String:
 		randStringVal := randString()
-		receiver.SetString(randStringVal)
+		val.SetString(randStringVal)
 
 	case reflect.Slice:
-		elementType := receiver.Type().Elem()
+		elementType := val.Type().Elem()
 		size := 1 + rand.Intn(16)
 		sliceVal := reflect.MakeSlice(reflect.SliceOf(elementType), 0, size)
 		for i := 0; i < size; i++ {
@@ -68,17 +76,17 @@ func fillRandomly(receiver reflect.Value) error {
 			}
 			sliceVal = reflect.Append(sliceVal, newElement)
 		}
-		receiver.Set(sliceVal)
+		val.Set(sliceVal)
 
 	case reflect.Array:
-		for i := 0; i < receiver.Len(); i++ {
-			if err := fillRandomly(receiver.Index(i)); err != nil {
+		for i := 0; i < val.Len(); i++ {
+			if err := fillRandomly(val.Index(i)); err != nil {
 				return err
 			}
 		}
 
 	case reflect.Map:
-		mapType := receiver.Type()
+		mapType := val.Type()
 		mapVal := reflect.MakeMap(mapType)
 		size := 1 + rand.Intn(16)
 		for i := 0; i < size; i++ {
@@ -92,16 +100,16 @@ func fillRandomly(receiver reflect.Value) error {
 			}
 			mapVal.SetMapIndex(newKey, newElement)
 		}
-		receiver.Set(mapVal)
+		val.Set(mapVal)
 
 	case reflect.Struct:
-		for i := 0; i < receiver.NumField(); i++ {
-			if err := fillRandomly(receiver.Field(i)); err != nil {
+		for i := 0; i < val.NumField(); i++ {
+			if err := fillRandomly(val.Field(i)); err != nil {
 				return err
 			}
 		}
 	default:
-		return fmt.Errorf("unsupported kind: %s", receiver.Kind().String())
+		return fmt.Errorf("unsupported kind: %s", val.Kind().String())
 	}
 	return nil
 }
